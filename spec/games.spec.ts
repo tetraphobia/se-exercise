994d0f17ecdbef7ff077234e2c3870ad9fc9266b
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-03-11 00:21:52 -0500

    test: wrap mongo connection in before hook

--- spec/games.spec.ts
+++ spec/games.spec.ts
@@ -2,11 +2,12 @@ import request from 'supertest'
 import { expect } from 'chai'
 import { server } from '../src/server'
 import { mongoConnect } from '../src/helpers/db'
-
-// Connect to separate test database
-mongoConnect()
+import { IGame } from '../src/interfaces/game.interface'
 
 describe('API endpoints', () => {
+  before('Connect to MongoDB', () => {
+    mongoConnect()
+  })
   describe('GET /games', () => {
     it('responds 200 OK', async () => {
       const res = await request(server)
@@ -15,6 +16,32 @@ describe('API endpoints', () => {
         .set('Content-Type', 'application/json')
 
       expect(res.statusCode).to.equal(200)
+    })
+  })
+
+  describe('POST /games', () => {
+    it('responds 201 Created', async () => {
+      // Generate a test body
+      const body: IGame = {
+        board: Array.from(Array(100).fill(0), () => new Array(100).fill(0)),
+        player: {
+          health: 200,
+          moves: 450,
+          pos: {
+            x: 0,
+            y: 0
+          }
+        }
+      }
+
+      const res = await request(server)
+        .post('/games')
+        .send(body)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+
+      expect(res.statusCode).to.equal(201)
+      expect(res.body).to.have.property('id')
     })
   })
 })
